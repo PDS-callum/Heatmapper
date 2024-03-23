ef60746d37bf51314eb06ccfe759971e6667f24c
PDS-callum <cpmwaller@gmail.com>
2024-03-23 14:46:40 +0000

    updates

---
+++ mapper.py
@@ -0,0 +1,11 @@
+import yaml
+
+# Open the configuration file
+with open("configs/config.yaml", 'r') as file:
+    config = yaml.safe_load(file)
+
+def main(config):
+    
+
+if __name__ == "__main__":
+    main()
